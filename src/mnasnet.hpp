d6872b48b7aaceca1d31f87cca30e608bff91475
yytdfc <fuchen@foxmail.com>
2019-02-05 23:44:39 +0800

    add classifier

--- src/mnasnet.hpp
+++ src/mnasnet.hpp
@@ -4,13 +4,14 @@ using namespace std;
 
 //in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True
 auto Conv2d(size_t in, size_t out, size_t kernel, size_t stride=1, size_t padding=0, size_t dilation=1, size_t groups=1, bool bias=true){
-  auto opt = torch::nn::Conv2dOptions(in, out, kernel);
-  opt.stride(stride);
-  opt.padding(padding);
-  opt.dilation(dilation);
-  opt.groups(groups);
-  opt.with_bias(bias);
-  return torch::nn::Conv2d(opt);
+  return torch::nn::Conv2d(
+      torch::nn::Conv2dOptions( in, out, kernel)
+      .stride(stride)
+      .padding(padding)
+      .dilation(dilation)
+      .groups(groups)
+      .with_bias(bias)
+      );
 }
 
 auto Conv3x3(size_t in, size_t out, size_t stride=1, size_t dilation=1, size_t groups=1){
@@ -86,24 +87,29 @@ struct MnasNet : torch::nn::Module {
     {6, 320, 1, 1, 3},  // -> 7x7
   };
   vector<torch::nn::Sequential> layers;
+  torch::nn::Sequential classifier;
 
   MnasNet(size_t n_class=1000, float width_mult=1.0) {
     size_t input_channel = 32 * width_mult;
+    size_t last_channel = 1280 * width_mult;
     layers.emplace_back(register_module("layer0", Conv3x3(/*in*/ 3, /*out*/ input_channel, /*stride*/ 2)));
     layers.emplace_back(register_module("layer1", SepConv3x3(/*in*/ input_channel, /*out*/ 16)));
     input_channel = 16;
     for(size_t i=0; i!=6; ++i){
+      size_t expand_ratio = config[i][0];
       size_t output_channel = config[i][1];
+      size_t n = config[i][2];
       size_t stride = config[i][3];
+      size_t kernel = config[i][4];
       torch::nn::Sequential layer;
-      for(size_t j=0; j!=config[i][2]; ++j){
+      for(size_t j=0; j!=n; ++j){
         shared_ptr<InvertedResidual> l(
             new InvertedResidual(
               input_channel,
               output_channel,
-              /*kernel*/ config[i][4],
+              /*kernel*/ kernel,
               /*stride*/ stride,
-              /*expand_ratio*/ config[i][0]
+              /*expand_ratio*/ expand_ratio
               ));
         layer->push_back(l);
         input_channel = output_channel;
@@ -111,23 +117,29 @@ struct MnasNet : torch::nn::Module {
       }
       layers.emplace_back(move(register_module("layer" + to_string(i+2), layer)));
     }
+    classifier = register_module("classifier", torch::nn::Sequential(
+          Conv2d(input_channel, last_channel, /*kernel*/ 1, /*stride*/ 1, /*padding*/ 0, /*dilation*/ 1, /*groups*/ 1, false),
+          torch::nn::Functional(torch::adaptive_avg_pool2d, torch::IntList({1,1})),
+          Conv2d(last_channel, n_class, /*kernel*/ 1, /*stride*/ 1, /*padding*/ 0, /*dilation*/ 1, /*groups*/ 1, false)
+          ));
+  }
 
+  vector<torch::Tensor> features(torch::Tensor x) {
+    vector<torch::Tensor> out;
+    for(auto& layer: layers){
+      x = layer->forward(x);
+      out.push_back(x);
+    }
+    return out;
   }
 
   torch::Tensor forward(torch::Tensor x) {
-    for(auto& layer: layers){
-      x = layer->forward(x);
-    }
-    return x;
-    // auto l0 = layers[0]->forward(x);
-    // auto l1 = layers[1]->forward(l0);
-    // auto l2 = layers[2]->forward(l1);
-    // auto l3 = layers[3]->forward(l2);
-    // auto l4 = layers[4]->forward(l3);
-    // auto l5 = layers[5]->forward(l4);
-    // auto l6 = layers[6]->forward(l5);
-    // auto l7 = layers[7]->forward(l6);
-    // return l7;
+     x = features(x).back();
+     return classifier->forward(x);
+  }
+
+  torch::Tensor loss(torch::Tensor x) {
+    return features(x).back();
   }
 };
 }
--- src/test.cc
+++ src/test.cc
@@ -8,7 +8,7 @@ int main() {
   // auto conv3x3 = Conv1x1(3, 1);
   // auto conv3x3 = SepConv3x3(3, 1);
   // auto conv3x3 = InvertedResidual([>in*/ 3, /*out*/ 1, /*kernel*/ 1, /*stride*/ 1, /*expand_ratio<] 6);
-  auto conv3x3 = MnasNet();
+  auto conv3x3 = MnasNet(10);
   cout << conv3x3 <<endl;
 
   // Create a new Net.
@@ -17,5 +17,7 @@ int main() {
 
   torch::Tensor tensor = torch::rand({1, 3, 256, 256});
 
+  for(auto& l : conv3x3.features(tensor))
+    std::cout << l.sizes() << std::endl;
   std::cout << conv3x3.forward(tensor).sizes() << std::endl;
 }
